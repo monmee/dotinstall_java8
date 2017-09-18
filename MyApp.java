980ef6ea56076871e4934546628bc038ea605576
vagrant <vagrant@localhost.localdomain>
2017-09-18 07:02:23 +0200

    05 さまざまなデータ型を使おう

--- MyApp.java
+++ MyApp.java
@@ -1,7 +1,18 @@
 public class MyApp {
   public static void main(String[] args) {
-    String msg;
-    msg = "Hello world, again!";
+    char a = 'a';
+    // 整数 byte, short, int, long
+    int x = 10;
+    long y = 5555555555555555L;
+    // 浮動小数点 float, double
+    double d = 2342.3432;
+    float f = 234.123F;
+    // 論理値
+    boolean flag = true; // false
+    // 文字列
+    // \n
+    // \t
+    String msg = "Hello Wo\nrld Again\t!";
     System.out.println(msg);
   }
 }
