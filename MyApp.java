8c1ea93ac4869f51aca42456024352d92e5de478
vagrant <vagrant@localhost.localdomain>
2017-09-18 07:05:31 +0200

    06 データの演算をしてみよう

--- MyApp.java
+++ MyApp.java
@@ -1,18 +1,26 @@
 public class MyApp {
   public static void main(String[] args) {
-    char a = 'a';
-    // 整数 byte, short, int, long
-    int x = 10;
-    long y = 5555555555555555L;
-    // 浮動小数点 float, double
-    double d = 2342.3432;
-    float f = 234.123F;
-    // 論理値
-    boolean flag = true; // false
-    // 文字列
-    // \n
-    // \t
-    String msg = "Hello Wo\nrld Again\t!";
-    System.out.println(msg);
+    // 演算
+    // + - * / %
+    // ++ --
+    // int i;
+    // i = 10 / 3;
+    // System.out.println(i); // 3
+    // i = 10 % 3;
+    // System.out.println(i); // 1
+    // int x = 5;
+    // x++;
+    // System.out.println(x); // 6
+    // x--;
+    // System.out.println(x); // 5
+
+    // int x = 5;
+    // // x = x + 12;
+    // x += 12;
+    // System.out.println(x);
+
+    String s;
+    s = "hello " + "world";
+    System.out.println(s);
   }
 }
