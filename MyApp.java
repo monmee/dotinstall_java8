82781800c3b4b7d25f953c64346e4ce6c77faa16
vagrant <vagrant@localhost.localdomain>
2017-09-23 22:35:52 +0200

    20 クラスを継承してみよう

--- MyApp.java
+++ MyApp.java
@@ -6,25 +6,38 @@ class User {
     this.name = name;
   }
 
-  // overload
-  User() {
-    // this.name = "Me";
-    this("Me"); //Userの引数にMeを入れているのでconstructorが呼び出される．結果，"Hi, Me"が出力される．
-  }
-
   void sayHi() {
     System.out.println("Hi, " + this.name);
   }
 }
 
+class AdminUser extends User {
+  AdminUser(String name) {
+    super(name); // 親クラスのconstructor
+
+  }
+
+  void sayHello() {
+    System.out.println("Hello, " + this.name);
+  }
+
+  @Override // アノテーション．メソッド名や引数が間違っているとエラーにしてくれる．
+  void sayHi() {
+    System.out.println("[admin] Hi, " + this.name);
+  }
+
+}
 public class MyApp {
   public static void main(String[] args) {
 
     // class
-    User tom;
-    tom = new User(); // インスタンス
-
+    User tom = new User("Tom");
     System.out.println(tom.name);
     tom.sayHi();
+
+    AdminUser bob = new AdminUser("Bob");
+    System.out.println(bob.name);
+    bob.sayHi();
+    bob.sayHello();
   }
 }
