56549eb77cbc6daf2dc0b78442f44c094181897c
monmee <jazzmonmee@gmail.com>
2017-09-23 16:41:45 +0900

    15 基本データ型と参照型を理解しよう

--- MyApp.java
+++ MyApp.java
@@ -1,14 +1,26 @@
 public class MyApp {
   public static void main(String[] args) {
-    // 配列
-    int[] sales = {700, 400, 500};
+    // int x = 10;
+    // int y = 5;
+    // System.out.println(x);
+    // System.out.println(y);
 
-    // for (int i = 0; i < sales.length; i++) {
-    //   System.out.println(sales[i]);
-    // }
 
-    for (int sale : sales) {
-      System.out.println(sale);
-    }
+    int[] a = {3, 5, 7};
+    int[] b = a;
+    b[1] = 8;
+    System.out.println(a[1]); // -> 8
+    System.out.println(b[1]); // -> 8
+    // 「int[] b = a」の時点で b には a が格納されているメモリ領域の番地が入るので,
+    //  a と b も同じ {3, 5, 7} のデータを指し示しているから．
+
+    String s = "hello";
+    String t = s;
+    t = "world";
+    System.out.println(s);
+    System.out.println(t);
+    // String型は参照型だが，文字列に関してはほぼ基本データ型と同じような動きを行う．
+    // 基本的に文字列は変更が不可になっていて、
+    // 違う文字列を割り当てると別の領域に新しくデータを確保するという仕組みになっているから
   }
 }
