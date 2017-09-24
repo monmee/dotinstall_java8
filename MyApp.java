c004a4551e01932fcfe21bc7273618c824fcc818
monmee <jazzmonmee@gmail.com>
2017-09-24 19:05:03 +0900

    28 抽象クラスを使ってみよう

--- MyApp.java
+++ MyApp.java
@@ -1,40 +1,32 @@
-final class User {
-  protected String name;
-  private static final double VERSION = 1.1; // 定数
+// 抽象クラス > 具象クラス
 
-  // constructor
-  public User(String name) {
-    this.name = name;
-    User.VERSION = 1.2;
-  }
 
-  public final void sayHi() {
-    System.out.println("Hi, " + this.name);
-  }
+
+abstract class User {
+  public abstract void sayHi(); // 抽象メソッド
 }
 
-public class AdminUser extends User {
-  public AdminUser(String name) {
-    super(name); // 親クラスのconstructor
-  }
-
-  public void sayHello() {
-    System.out.println("Hello, " + this.name);
-  }
-
-  @Override // アノテーション．メソッド名や引数が間違っているとエラーにしてくれる．
+class JapaneseUser extends User {
+  @Override
   public void sayHi() {
-    System.out.println("[admin] Hi, " + this.name);
+    System.out.println("こんにちは");
   }
 
+}
+
+
+class AmericanUser extends User {
+  @Override
+  public void sayHi() {
+    System.out.println("Hi");
+  }
 }
 
 public class MyApp {
   public static void main(String[] args) {
-    User.getInfo(); // 0
-    User tom = new User("tom");
-    User.getInfo(); // 1
-    User bob = new User("bob");
-    User.getInfo(); // 2
+    JapaneseUser nissy = new JapaneseUser();
+    AmericanUser tom = new AmericanUser();
+    nissy.sayHi();
+    tom.sayHi();
   }
 }
